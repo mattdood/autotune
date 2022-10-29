3cd3277d0d0f0b03e9f7650d49b2f5552a4e69b6
mattdood <mawimberly@csumb.edu>
2022-10-28 18:51:55 -0700

    Refactor postgres + rds.tf with new modules config

--- modules/autotune-rds/outputs.tf
+++ modules/autotune-rds/outputs.tf
@@ -3,66 +3,66 @@
 
 output "address" {
   description = "RDS hostname"
-  value = aws_instance.postgres.address
+  value       = aws_instance.postgres.address
 }
 
 output "arn" {
   description = "ARN of the RDS instance"
-  value = aws_instance.postgres.arn
+  value       = aws_instance.postgres.arn
 }
 
 output "availability_zone" {
   description = "RDS availability zone"
-  value = aws_instance.postgres.availability_zone
+  value       = aws_instance.postgres.availability_zone
 }
 
 output "endpoint" {
   description = "Endpoint of the Postgres database instance"
-  value = aws_instance.postgres.endpoint
+  value       = aws_instance.postgres.endpoint
 }
 
 output "engine_version_actual" {
   description = "RDS actual engine version"
-  value = aws_instance.postgres.engine_version_actual
+  value       = aws_instance.postgres.engine_version_actual
 }
 
 output "hosted_zone_id" {
   description = "RDS hosted zone ID"
-  value = aws_instance.postgres.hosted_zone_id
+  value       = aws_instance.postgres.hosted_zone_id
 }
 
 output "instance_class" {
   description = "RDS instance class"
-  value = aws_instance.postgres.instance_class
+  value       = aws_instance.postgres.instance_class
 }
 
 output "multi_az" {
   description = "If the RDS is multi AZ enabled"
-  value = aws_instance.postgres.multi_az
+  value       = aws_instance.postgres.multi_az
 }
 
 output "name" {
   description = "Name of the created database"
-  value = aws_instance.postgres.name
+  value       = aws_instance.postgres.name
 }
 
 output "port" {
   description = "Port of the created database"
-  value = aws_instance.postgres.port
+  value       = aws_instance.postgres.port
 }
 
 output "resource_id" {
   description = "Resource ID of the RDS instance"
-  value = aws_instance.postgres.port
+  value       = aws_instance.postgres.port
 }
 
 output "tags" {
   description = "Resource tags map of the RDS instance"
-  value = aws_instance.postgres.tags_all
+  value       = aws_instance.postgres.tags_all
 }
 
 output "username" {
   description = "Master database username"
-  value = aws_instance.postgres.username
+  value       = aws_instance.postgres.username
 }
 
--- modules/autotune-rds/rds.tf
+++ modules/autotune-rds/rds.tf
@@ -1,41 +1,31 @@
-# RDS database configuration
-############################
-# This is the core of the Postgres module and contains the instance
-# itself. Workload specific configurations are stored under the
-# associated workload file. E.g. olap.tf, oltp.tf, hybrid.tf.
-#
-# Note: This file is meant to be a baseline start for a database,
-# it does not contain all settings that can be used (e.g., multi-az,
-# allocated storage, network type, etc.).
-#
-# References:
-#   * Database parameter references - A great site for the database parameter descriptions
-#     and which engine version a parameter was created in
-#     * https://postgresqlco.nf/doc/en/param/
-#   * PGTune - A site for creating some default values for Postgres parameters.
-#     * https://pgtune.leopard.in.ua/
+# RDS module
+#######################
+# Contains all the requirements to create an RDS instance with sane defaults.
+# This needs to have parameters pushed in, though defaults are provided for
+# most settings. Workload and engine specification will pull in sub-moduels
+# to generate parameter groups and create the RDS instance.
 
-resource "aws_db_instance" "postgres" {
-  allocated_storage = var.allocated_storage
+resource "aws_db_instance" "autotune-rds" {
+  allocated_storage           = var.allocated_storage
   allow_major_version_upgrade = false
-  apply_immediately = var.apply_immediately
-  auto_minor_version_upgrade = false
-  availability_zone = var.availability_zone
-  backup_window = var.backup_window
-  copy_tags_to_snapshot = true
-  db_name = var.db_name
-  engine = "postgres"
-  engine_version = var.version
-  identifier = var.identifier
-  instance_class = var.instance_class
-  maintenance_window = var.maintenance_window
-  max_allocated_storage = var.max_allocated_storage
-  parameter_group_name = var.parameter_group_type
-  password = var.password
-  publicly_accessible = var.publicly_accessible
-  port = var.port
-  tags = var.tags
-  username = var.username
-  vpc_security_group_ids = var.vpc_security_group_ids
+  apply_immediately           = var.apply_immediately
+  auto_minor_version_upgrade  = false
+  availability_zone           = var.availability_zone
+  backup_window               = var.backup_window
+  copy_tags_to_snapshot       = true
+  db_name                     = var.db_name
+  engine                      = var.engine
+  engine_version              = var.version
+  identifier                  = var.identifier
+  instance_class              = var.instance_class
+  maintenance_window          = var.maintenance_window
+  max_allocated_storage       = var.max_allocated_storage
+  parameter_group_name        = var.parameter_group_type
+  password                    = var.password
+  publicly_accessible         = var.publicly_accessible
+  port                        = var.port
+  tags                        = var.tags
+  username                    = var.username
+  vpc_security_group_ids      = var.vpc_security_group_ids
 }
 
