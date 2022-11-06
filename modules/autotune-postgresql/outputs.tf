c40097074b15e4e24fa1a000a22d415aa9327866
mattdood <mawimberly@csumb.edu>
2022-11-06 13:36:17 -0800

    Postgres: Dynamic outputs for workload type

--- modules/autotune-postgresql/outputs.tf
+++ modules/autotune-postgresql/outputs.tf
@@ -1,1 +1,24 @@
+#######################
+# RDS database outputs
+#######################
+
+output "parameter_group_arn" {
+  description = "Parameter group ARN for the Autotune Postgres RDS instances"
+  value = (
+    length(aws_db_parameter_group.hybrid) > 0 ? aws_db_parameter_group.hybrid.arn
+    : length(aws_db_parameter_group.olap) > 0 ? aws_db_parameter_group.olap.arn
+    : length(aws_db_parameter_group.oltp) > 0 ? aws_db_parameter_group.oltp.arn
+    : ""
+  )
+}
+
+output "parameter_group_name" {
+  description = "Parameter group name for the Autotune Postgres RDS instances"
+  value = (
+    length(aws_db_parameter_group.hybrid) > 0 ? aws_db_parameter_group.hybrid.id
+    : length(aws_db_parameter_group.olap) > 0 ? aws_db_parameter_group.olap.id
+    : length(aws_db_parameter_group.oltp) > 0 ? aws_db_parameter_group.oltp.id
+    : ""
+  )
+}
 
