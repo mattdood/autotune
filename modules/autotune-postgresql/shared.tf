29e6eb4e1379c4f5fbf0faf7071c87402750aa88
mattdood <mawimberly@csumb.edu>
2022-11-05 18:07:01 -0700

    Postgres-OLAP: Refactor to use parameter values provided by AWS (e.g., DBInstanceVCPU) rather than map(string) values

--- modules/autotune-postgresql/shared.tf
+++ modules/autotune-postgresql/shared.tf
@@ -2,8 +2,8 @@
 data "aws_db_parameter_group" "shared_settings" {
   logging = [
     {
-      name = ""
-      value = ""
+      name         = ""
+      value        = ""
       apply_method = ""
     },
     {
